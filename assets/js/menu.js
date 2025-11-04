98004d67910efed87af1db1cee0d92edabcf48a7
mochichoco <g.kanon27@gmail.com>
2025-11-04 10:07:17 +0900

    Create menu.js

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.site-nav');
+  if(menuBtn && nav){
+    menuBtn.addEventListener('click', () => {
+      nav.classList.toggle('open');
+      menuBtn.classList.toggle('active');
+    });
+  }
+});
